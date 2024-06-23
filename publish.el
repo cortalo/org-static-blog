2fda6f0d03542785dddb6b29e8ce18966e5d36e6
chenyo-17 <cy970802@hotmail.com>
2024-06-23 18:07:14 +0200

    added first post

--- publish.el
+++
@@ -1,7 +0,0 @@
-;; org-static-blog config
-(setq org-static-blog-publish-title "org-static-blog")
-(setq org-static-blog-publish-url "https://chenyo-17.github.io/org-static-blog")
-(setq org-static-blog-publish-directory "~/blog/")
-(setq org-static-blog-posts-directory "~/blog/posts/")
-(setq org-static-blog-drafts-directory "~/blog/drafts/")
-(setq org-static-blog-enable-tags t)
